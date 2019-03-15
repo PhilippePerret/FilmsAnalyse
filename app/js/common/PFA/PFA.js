894c355024bfb4772ea0da3a4a2064c4d16400e0
Philippe Perret <philippe.perret@yahoo.fr>
2019-03-15 09:14:17 +0300

    Calcul efficient de la zone de chaque noeud structurel (testé)

--- app/js/common/PFA/PFA.js
+++ app/js/common/PFA/PFA.js
@@ -3,7 +3,6 @@
 const PFA = {
     class: 'PFA'
   , DATA_STT_NODES: {
-        // TODO : Faire plutôt des instances de SttNode
         // cZone définit le calcul de la zone où le noeud peut se trouver
         // Noter qu'on peut indiquer "<identifiant>" pour ne pas préciser un
         // temps mais préciser le noeud qui servira de fin ou de début
@@ -13,7 +12,7 @@ const PFA = {
       , preamb: {hname: 'Préambule', shortHname: 'Préamb.', cZone:'[0,huiti]'}
       , incPer: {hname: 'Incident perburbateur', shortHname: 'Inc.Perturb.', cZone:'[0,douzi]'}
       , incDec: {hname: 'Incident déclencheur', shortHname: 'Inc.Déc.', cZone:'[iem24,quart-iem24]'}
-      , zone_r: {hname: 'Zone de refus', shortHname: 'Zone R.', cZone:'["incDec","pivot1",quart-huiti,quart]'}
+      , zone_r: {hname: 'Zone de refus', shortHname: 'Zone R.', cZone:'["incDec","pivot1"]'}
       , pivot1: {hname: 'Pivot 1', shortHname: 'Pvt 1', cZone: '[quart-iem24,quart]'}
       , DEV1:   {hname: 'DÉVELOPPEMENT (1ère partie)', shortHname: 'DÉV. Part 1', cZone:'[quart,moiti]'}
       , tiers1: {hname: 'Premier Tiers', shortHname: '1/3', cZone:'[tiers-iem24,tiers+iem24]'}
@@ -21,43 +20,50 @@ const PFA = {
       , DEV2:   {hname: 'DÉVELOPPEMENT (2nde partie)', shortHname: 'DÉV. Part 2', cZone:'[moiti,tresQ]'}
       , tiers1: {hname: 'Second Tiers', shortHname: '2/3', cZone:'[deuxT-iem24,deuxT+iem24]'}
       , pivot2: {hname: 'Pivot 2', shortHname: 'Pvt 2', cZone:'[tresQ-iem24,tresQ]'}
-      , DENOU:  {hname: 'DÉNOUEMENT', shortHname: 'DÉNOUE.', cZone:'[tresQ,duree]'}
+      , DNOU:   {hname: 'DÉNOUEMENT', shortHname: 'DÉNOUE.', cZone:'[tresQ,duree]'}
       , crisis: {hname: 'Crise', shortHname: 'Crise', cZone:'[tresQ,"climax"]', cZoneAlt:'[tresQ-douzi,tresQ]'}
       , climax: {hname: 'Climax', shortHname: 'Climax', cZone:'[duree-huiti,duree]'}
       , desine: {hname: 'Désinence', shortHname:'Désin.', cZone:'["climax",duree]'}
     }
 
-    /**
-     * Méthode qui calcule les positions absolues des noeuds et parties en
-     * fonction du temps du film.
-     */
-  , calcAbsolutePositions:function(){
-      console.log("-> calcAbsolutePositions")
-      if(undefined === current_analyse.duration) return
+    // ---------------------------------------------------------------------
+    //  Méthodes de données
 
-      // Durée et divisions, pour calculer les zones des Nœuds
-      var duree = current_analyse.duration // nom plus court + calcul
-      var moiti = parseInt(duree/2,10)
-      var quart = parseInt(duree/4,10)
-      var tresQ = parseInt(3*duree/4,10)
-      var huiti = parseInt(duree/8,10)
-      var douzi = parseInt(duree/12,10)
-      var iem24 = parseInt(duree/24,10)
-      var tiers = parseInt(duree/3,10)
-      var deuxT = parseInt(2*duree/3,10)
-
-      for(var nid in this.DATA_STT_NODES){
-        var node = this.DATA_STT_NODES[nid]
-        node.zone = eval(node.cZone)
-        if(node.cZoneAlt) node.zoneAlt = eval(node.cZoneAlt)
+      /**
+       * Retourne l'instance SttNode du noeud d'identifiant +nid+
+       *
+       * Note : +nid+ est une des clés de DATA_STT_NODES (cf. ci-dessus)
+       */
+    , node:function(nid){
+        if(undefined === this.nodes) this.nodes = {}
+        if(undefined === this.nodes[nid]){
+          this.nodes[nid] = new SttNode(nid, this.DATA_STT_NODES[nid])
+        }
+        return this.nodes[nid]
       }
-      console.log(this.DATA_STT_NODES)
-    }
+
+    // ---------------------------------------------------------------------
+    //  Méthodes d'entrée sorties
+    , saveIfModified:function(){
+        this.modified && this.save()
+      }
+    , save:function(){
+        F.error("La procédure d'enregistrement du PFA n'est pas encore implémentée")
+        return new Promise((ok,ko) => {
+          ok()
+        })
+      }
+    // ---------------------------------------------------------------------
+    //  Méthodes de calculs
 
 }
 Object.defineProperties(PFA,{
+    modified:{
+        get:function(){return this._modified}
+      , set:function(v){this._modified = v}
+    }
     //Retourne la class SttNode de l'incident perturbateur
-    incPer:{
+  , incPer:{
         get:function(){return this._incPer}
       , set:function(e){this._incPer = e}
     }
--- app/js/common/PFA/SttNode.js
+++ app/js/common/PFA/SttNode.js
@@ -5,10 +5,127 @@
  *  ----------------
  *  Pour la gestion des noeud structurels
  */
-class SttNode extends FAEvent {
-  constructor(){
+class SttNode {
 
+  // ---------------------------------------------------------------------
+  //  CLASS
+
+  // Calcule et retourne la zone à partir du String +czone+
+  // Cf. comment +czone+ est défini dans PFA.DATA_STT_NODES
+  //
+  // +nid+ sert uniquement pour les dépendances
+  static calcZone(sttnode){
+    var czone = sttnode.cZone
+    if(undefined === current_analyse.duration) return
+    // Durée et divisions, pour calculer les zones des Nœuds
+    var duree = this._duree || this.initDuree('duree', current_analyse.duration)
+    var moiti = this._moiti || this.initDuree('moiti', duree/2)
+    var quart = this._quart || this.initDuree('quart', duree/4)
+    var tresQ = this._tresQ || this.initDuree('tresQ', 3*duree/4)
+    var huiti = this._huiti || this.initDuree('huiti', duree/8)
+    var douzi = this._douzi || this.initDuree('douzi', duree/12)
+    var iem24 = this._iem24 || this.initDuree('iem24', duree/24)
+    var tiers = this._tiers || this.initDuree('tiers', duree/3)
+    var deuxT = this._deuxT || this.initDuree('deuxT', 2*duree/3)
+
+    var z = eval(czone)
+
+    /*
+    * Les deux valeurs de z peut être soit des nombres, soit des
+    * string. Quand ce sont des strings, ils font références à d'autres
+    * noeuds qui doivent donner leur position, soit de façon absolue (si non
+    * définis) soit de façon relative (si défini)
+    * [2] Si les valeurs sont identiques (ce qui arrive, on élargit la zone d'un
+    * 12ième de chaque côté)
+    */
+    var refNode
+    if('string' === typeof z[0]){
+      refNode = current_analyse.PFA.node(z[0])
+      z[0] = refNode.endAt || refNode.zoneEnd
+      // On indique la dépendance, pour reseter en cas de redéfinition
+      refNode.dependencies.push(sttnode.id)
+    }
+    if('string' === typeof z[1]){
+      refNode = current_analyse.PFA.node(z[1])
+      z[1] = refNode.startAt || refNode.zoneStart
+      refNode.dependencies.push(sttnode.id)
+    }
+    // [2]
+    if(z[0] == z[1]){
+      z[0] = z[0] - iem24
+      z[1] = z[1] + iem24
+    }
+    return z
   }
+  static initDuree(kduree, value){
+    this[`_${kduree}`] = parseInt(value,10)
+    return value
+  }
+
+
+  // ---------------------------------------------------------------------
+  //  INSTANCE
+  constructor(nid, data){
+    this.id = nid
+    for(var p in data){this[`_${p}`] = data[p]}
+    // Par exemple, on définit `this._cZone`
+
+    // Les nœuds dépendants de ce noeud (au niveau des temps). Ils seront
+    // définis dans la méthode de classe `calcZone`
+    this.dependencies = []
+  }
+
+
+  // ---------------------------------------------------------------------
+  //  Méthodes de données fixes (absolues)
+  get cZone(){ return this._cZone }
+
+
+  // ---------------------------------------------------------------------
+  //  Méthodes de données relatives (du film courant)
+
+  /**
+   * ID de l'event associé
+   */
+  // Note : pas de `set`, on utilise `this.event = `
+  get event_id(){ return this._event_id}
+
+  get startAt() { return this._startAt }
+  set startAt(v){ this._startAt = v ; this.resetDependencies() }
+  get endAt()   { return this._endAt }
+  set endAt(v)  { this._endAt = v ; this.resetDependencies() }
+
+  // ---------------------------------------------------------------------
+  //  Méthodes de données volatiles
+
+  get modified(){return this._modified }
+  set modified(v){this._modified = true ; current_analyse.PFA.modified = true }
+
+  reset(props){
+    if(undefined===props)props=['zone']
+    for(var i=0,len=props.length;i<len;++i){this[`_${props[i]}`] = undefined}
+  }
+
+  get zone(){return this._zone || defineP(this,'_zone',SttNode.calcZone(this))}
+
+  get zoneStart() { return this.zone[0] }
+  get zoneEnd()   { return this.zone[1] }
+
+  get scene(){ return this._scene }
+
+  get event(){return this._event || defineP(this,'_event',current_analyse.ids[this.event_id])}
+  set event(v){this._event = v ; this._event_id = v.id ; this.modified = true }
+
+  resetDependencies(){
+    for(var i=0,len = this.dependencies.length;i<len;++i){
+      current_analyse.PFA.node(this.dependencies[i]).reset(['zone'])
+    }
+    this.modified = true
+  }
+
+  // ---------------------------------------------------------------------
+  // Méthodes de calculs
+
 
 }
 
